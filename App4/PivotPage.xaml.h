75d57b9a0579f06a017a770d7a85d295433757b6
matthew gold <mgold1@email.arizona.edu>
2016-01-24 07:30:12 -0700

    m
    n

---
+++ App4/PivotPage.xaml.h
@@ -0,0 +1,82 @@
+﻿//
+// PivotPage.xaml.h
+// Declaration of the PivotPage class.
+//
+
+#pragma once
+
+#include "PivotPage.g.h"
+
+namespace App4
+{
+	public ref class PivotPage sealed
+	{
+		Platform::String^ GetFirstGroupName() const
+		{
+			return ref new Platform::String(L"FirstGroup");
+		}
+
+		Platform::String^ GetSecondGroupName() const
+		{
+			return ref new Platform::String(L"SecondGroup");
+		}
+
+	public:
+		PivotPage();
+	//	int getranNum();
+	//	void setRanNum(int i);
+	//	int get 
+		static void RegisterDependencyProperties();
+		static property Windows::UI::Xaml::DependencyProperty^ DefaultViewModelProperty
+		{
+			Windows::UI::Xaml::DependencyProperty^ get() { return _defaultViewModelProperty; }
+		}
+
+		static property Windows::UI::Xaml::DependencyProperty^ NavigationHelperProperty
+		{
+			Windows::UI::Xaml::DependencyProperty^ get() { return _navigationHelperProperty; }
+		}
+
+		/// <summary>
+		/// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+		/// </summary>
+		property Common::NavigationHelper^ NavigationHelper
+		{
+			Common::NavigationHelper^ get();
+		}
+
+		/// <summary>
+		/// Gets the view model for this <see cref="Page"/>.
+		/// This can be changed to a strongly typed view model.
+		/// </summary>
+		property Windows::Foundation::Collections::IObservableMap<Platform::String^, Platform::Object^>^ DefaultViewModel
+		{
+			Windows::Foundation::Collections::IObservableMap<Platform::String^, Platform::Object^>^  get();
+		}
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+		virtual void OnNavigatedFrom(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+
+	private:
+		Windows::ApplicationModel::Resources::ResourceLoader^ _resourceLoader;
+
+		void SecondPivot_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void NavigationHelper_LoadState(Platform::Object^ sender, Common::LoadStateEventArgs^ e);
+		void NavigationHelper_SaveState(Platform::Object^ sender, Common::SaveStateEventArgs^ e);
+		void AddAppBarButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void ItemView_ItemClick(Platform::Object^ sender, Windows::UI::Xaml::Controls::ItemClickEventArgs^ e);
+
+		static Windows::UI::Xaml::DependencyProperty^ _defaultViewModelProperty;
+		static Windows::UI::Xaml::DependencyProperty^ _navigationHelperProperty;
+		void button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void PivotItem_LayoutUpdated(Platform::Object^ sender, Platform::Object^ e);
+		void slider_DragLeave(Platform::Object^ sender, Windows::UI::Xaml::DragEventArgs^ e);
+		void slider_ValueChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs^ e);
+		int players;
+		int ranNum;
+		int buttonPresses;
+		//static Windows::UI::Xaml::slider DependencyProperty^ GetValue
+		
+	};
+}
---
+++ App4/my_code.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+#include "PivotPage.xaml.h"
+#include "ItemPage.xaml.h"
+
